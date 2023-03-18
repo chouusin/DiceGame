0e95d97b546580fe4abea165078d6fed0ec50738
chouusin <chouusin457@gmail.com>
2023-03-18 15:52:04 +0900

    Add class Game to handle game logic

---
+++ game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, dice, board_values):
+        self.dice = dice
+        self.board_values = board_values
+
+        self.dice_value = self.board_values[0]
+        self.total_turns = 0
+
+    def run(self):
+        for board_value in self.board_values:
+            turns = self.dice.rotate_distance(self.dice_value, board_value)
+            self.total_turns += turns
+            self.dice_value = board_value
---
+++ tests/test_game.py
@@ -0,0 +1,28 @@
+import unittest
+
+from dice import Dice
+from game import Game
+
+
+class TestDice(unittest.TestCase):
+    test_data = [
+        {
+            'dice_values': [1, 6, 2, 5, 4, 3],
+            'board_values': [1, 5, 4, 3],
+            'expected_output': 4
+        },
+        {
+            'dice_values': [6, 5, 4, 3, 2, 1],
+            'board_values': [6, 2, 1, 3, 4, 3],
+            'expected_output': 8
+        }
+    ]
+
+    def test_total_turns(self):
+        for data in self.test_data:
+            dice = Dice(data['dice_values'])
+
+            game = Game(dice, data['board_values'])
+            game.run()
+
+            self.assertEqual(game.total_turns, data['expected_output'])
