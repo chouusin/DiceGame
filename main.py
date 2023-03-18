9d69e445989b5f6ab4c7bcdcc26fde66a90770bd
chouusin <chouusin457@gmail.com>
2023-03-18 16:07:58 +0900

    Add test for main

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from utils import parse_dice_values, parse_board_values
+from utils import parse_dice_values, parse_board_values, show_result
+from dice import Dice
+from game import Game
+
+
+def main():
+    dice_values = parse_dice_values()
+    dice = Dice(dice_values)
+
+    board_values = parse_board_values()
+
+    game = Game(dice, board_values)
+    game.run()
+
+    show_result(game.total_turns)
+
 
 if __name__ == '__main__':
-    dice_values = parse_dice_values()
-    board_values = parse_board_values()
+    main()
---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+import unittest
+from contextlib import redirect_stdout
+import io
+
+from main import main
+
+
+class TestMain(unittest.TestCase):
+    test_data = [
+        {
+            'input': ['1 6 2 5 4 3\n', '4\n', '1\n', '5\n', '4\n', '3\n'],
+            'expected_output': '4',
+        },
+        {
+            'input': ['6 5 4 3 2 1\n', '6\n', '6\n', '2\n', '1\n', '3\n', '4\n', '3\n'],
+            'expected_output': '8',
+        }
+    ]
+
+    def test_result(self):
+        for data in self.test_data:
+            with patch('builtins.input', side_effect=data['input']):
+                with io.StringIO() as captured_output:
+                    with redirect_stdout(captured_output):
+                        main()
+
+                    self.assertEqual(captured_output.getvalue(), data['expected_output'])
